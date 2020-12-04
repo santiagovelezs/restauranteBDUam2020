8e8d1c6fa015873e5dc32c9b3df1c4a011838516
santiago velez s <santivelezs@gmail.com>
2020-12-04 18:10:12 -0500

    e

---
+++ Backend/src/main/java/uam/bd/restaurante/DAOmysql/PedidoDAOImpl.java
@@ -0,0 +1,54 @@
+package uam.bd.restaurante.DAOmysql;
+
+import java.awt.List;
+import java.sql.Connection;
+
+import uam.bd.restaurante.DAO.DAO_Foreign;
+import uam.bd.restaurante.model.Pedido;
+
+public class PedidoDAOImpl implements DAO_Foreign<Pedido>
+{
+	private final Connection connection;
+
+	public PedidoDAOImpl(Connection connection)
+	{
+		this.connection = connection;
+	}
+
+	@Override
+	public List getAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Pedido getBy(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(Pedido t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Pedido t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Pedido t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public java.util.List<Pedido> getAllById(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
---
+++ Backend/src/main/java/uam/bd/restaurante/MysqlConnector/DBConnection.java
@@ -0,0 +1,80 @@
+package uam.bd.restaurante.MysqlConnector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;;
+
+public class DBConnection 
+{
+	private static Connection conn = null;
+
+	public static Connection getConnection() 
+	{
+		if (conn == null) 
+		{
+			String url = "jdbc:mysql://localhost/restaurante";	        
+	        Properties properties = new Properties();
+	        properties.put("user", "santi");
+	        properties.put("password", "1926");
+			try 
+			{				
+				conn = DriverManager.getConnection(url, properties);
+			} 
+			catch (SQLException e)
+			{
+				Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, e);
+			}
+		}
+		return conn;
+	}
+
+	public static void closeConnection() 
+	{
+		if (conn != null) 
+		{
+			try 
+			{
+				conn.close();
+			} 
+			catch (SQLException e) 
+			{
+				Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, e);
+			}
+		}
+	}	
+
+	public static void closeStatement(Statement st) 
+	{
+		if (st != null) 
+		{
+			try 
+			{
+				st.close();
+			} 
+			catch (SQLException e) 
+			{
+				Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, e);
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) 
+	{
+		if (rs != null)
+		{
+			try 
+			{
+				rs.close();
+			} 
+			catch (SQLException e) 
+			{
+				Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, e);
+			}
+		}
+	}
+}
