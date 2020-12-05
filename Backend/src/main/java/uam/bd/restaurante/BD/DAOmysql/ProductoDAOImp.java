1d80ef560fc0972a0f227657baee1d834ec0f353
santiago velez s <santivelezs@gmail.com>
2020-12-04 21:57:18 -0500

    fix

---
+++ Backend/src/main/java/uam/bd/restaurante/BD/DAOmysql/ProductoDAOImp.java
@@ -0,0 +1,50 @@
+package uam.bd.restaurante.BD.DAOmysql;
+
+import java.sql.Connection;
+import java.util.List;
+
+import uam.bd.restaurante.BD.DAO.DAO;
+import uam.bd.restaurante.BD.Model.Producto;
+
+public class ProductoDAOImp implements DAO<Producto>
+{
+
+	private final Connection connection;
+
+	public ProductoDAOImp(Connection connection) {
+		super();
+		this.connection = connection;
+	}
+
+	@Override
+	public List getAll() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Producto getBy(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean save(Producto t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Producto t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Producto t) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+}
