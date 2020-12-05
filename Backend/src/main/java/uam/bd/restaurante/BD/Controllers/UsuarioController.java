d2a72b1f7002537e52ff26f4cb116ca2ff41ddd9
santiagovelezs <santivelezs@gmail.com>
2020-12-05 15:59:07 -0500

    Perro Ciego

--- Backend/src/main/java/uam/bd/restaurante/BD/Controllers/UsuarioController.java
+++ Backend/src/main/java/uam/bd/restaurante/BD/Controllers/UsuarioController.java
@@ -3,10 +3,12 @@ package uam.bd.restaurante.BD.Controllers;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import uam.bd.restaurante.BD.DAO.DAO;
 import uam.bd.restaurante.BD.DAOmysql.UsuarioDAOImpl;
@@ -27,7 +29,18 @@ public class UsuarioController
 	public boolean saveUsuario(@RequestBody Usuario received) throws Exception
 	{
 		System.out.println("POST: "+received.getPassword());	
-		return usuarioDAO.save(received);		
+		try 
+		{
+			return usuarioDAO.save(received);
+		} 
+		catch (SQLIntegrityConstraintViolationException e) 
+		{
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Empleado con cédula "+received.getCedula()+" no existe", e);
+		} 
+		catch (Exception e) 
+		{
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Error, Exception ", e);
+		}		
 		
 	}
 	
